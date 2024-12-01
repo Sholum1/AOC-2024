7424b3953bbbf0079079684cd93c9a9f633eeacc
Wallysson Oliveira <wallyssonryu@gmail.com>
2024-12-01 17:15:48 -0300

    Second Half

--- Day 1/Day 1.clj
+++ Day 1/Day 1.clj
@@ -17,6 +17,24 @@
   [lst]
   (reduce + (map #(abs (- %1 %2)) (first lst) (second lst))))
 
-(defn -main
+(defn first-half
   []
   (result (left-right (parse (slurp file-input)))))
+
+(defn similarity
+  [num lst]
+  (count (filter #(= num %) (take-while #(<= % num) lst))))
+
+(defn left-right-sim
+  [[nums1 nums2]]
+  (->> nums1
+       (map #(vector % (similarity % nums2)))
+       (filter #(> (second %) 0))))
+
+(defn total-sim
+  [sims]
+  (reduce (fn [acc [num sim]] (+ (* num sim) acc)) 0 sims))
+
+(defn second-half
+  []
+  (total-sim (left-right-sim (left-right (parse (slurp file-input))))))
